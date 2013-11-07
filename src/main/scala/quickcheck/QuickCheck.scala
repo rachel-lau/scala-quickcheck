d0df41f4d20094fe9369c1a0eeaafecfecc7d610
Rachel Lau <rachel.yw.lau@gmail.com>
2013-11-07 14:17:06 -0800

    Added properties

--- src/main/scala/quickcheck/QuickCheck.scala
+++ src/main/scala/quickcheck/QuickCheck.scala
@@ -14,6 +14,29 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
+  /**
+   * If you insert any two elements into an empty heap, find the
+   * minimum of the resulting heap should get the smallest of the
+   * the two elements back.
+   */
+  property("insertTwo") = forAll { (n: Int, m: Int) =>
+    val h1 = insert(n, empty)
+    val h2 = insert(m, h1)
+    if (n <= m) 
+      findMin(h2) == n
+    else
+      findMin(h2) == m
+  }
+
+  /**
+   * If you insert an element into an empty heap, then delete the
+   * minimum, the resulting heap should be empty.
+   */
+  property("insertAndDelete") = forAll { n: Int =>
+    val h = insert(n, empty)
+    deleteMin(h) == empty
+  }
+
   lazy val genHeap: Gen[H] = ???
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
